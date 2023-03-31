5bf091a13d5cc8567c986968f4bbce47a3c20ea4
kylercantu <kylercantu99@gmail.com>
2023-03-31 16:44:41 -0400

    Added Functionality to "Label" Button

--- DesignHome.java
+++ DesignHome.java
@@ -1,8 +1,10 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.QuadCurve2D;
 import java.util.ArrayList;
 
 public class DesignHome extends JFrame{
@@ -23,12 +25,14 @@ public class DesignHome extends JFrame{
 
 
 
-    private ArrayList<Shape> shapes = new ArrayList<Shape>(); //Hold all lines (shapes) that are drawn
+    private ArrayList<Shape> shapes = new ArrayList<Shape>(); //Hold all shapes that are drawn
     private ArrayList<Color> colors = new ArrayList<Color>(); //Holds the colors of the shape
     private Point2D.Float drawStart = null; //The starting point of the drawn shape
     private Point2D.Float drawEnd = null; //The end point of the drawn shape
+    private  Point2D.Float controlPoint = null; //The point used to control the curve of an arc
     private int currentAction; //Variable that represents which button is pressed/active at the moment
     private Color color;
+
 
 
 
@@ -43,6 +47,7 @@ public class DesignHome extends JFrame{
         setContentPane(backgroundPanel);
         setLocationRelativeTo(null);
         setResizable(false);
+        drawPanel.setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }//End setFrame
@@ -54,7 +59,7 @@ public class DesignHome extends JFrame{
                 dispose();
                 HomePage homePage = new HomePage();
             }
-        });//END backBtn ActionListener
+        });//End backBtn ActionListener
 
         lineBtn.addActionListener(new ActionListener() {
             @Override
@@ -82,13 +87,37 @@ public class DesignHome extends JFrame{
             }
         });//End lineBtn ActionListener
 
+        labelBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentAction = 4;
+                addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if(currentAction == 4) {
+                            int xPos = e.getX() - 52;
+                            int yPos = e.getY() - 100;
+
+                            String roomName = JOptionPane.showInputDialog("Enter room name:");
+                            JButton roomBtn = new JButton(roomName);
+                            roomBtn.setFont(new Font("Arial", Font.BOLD, 10));
+                            roomBtn.setLocation(xPos, yPos);
+                            roomBtn.setSize(90,30);
+                            drawPanel.add(roomBtn);
+                            repaint();
+                        }
+                    }
+                });
+            }
+        });//End labelBtn ActionListener
+
+
         colorBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 currentAction = 5;
                 JColorChooser colorChooser = new JColorChooser();
                 color = JColorChooser.showDialog(null, "Color Picker", Color.BLACK);
-
             }
         });//End colorBtn ActionListener
 
@@ -106,7 +135,6 @@ public class DesignHome extends JFrame{
             }
         });//End undoBtn ActionListener
 
-        
     }//End btnConfig method
 
     @Override
